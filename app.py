6a4c85a3248d21247057b4111d09152a240c8a81
Subashree-selvaraj <subashreevjc@gmail.com>
2024-05-18 15:09:42 +0530

    main

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    image = request.files['image']
-    image.save('/path/to/save/image')
-    return 'Image uploaded successfully'
